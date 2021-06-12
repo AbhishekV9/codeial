eee9211a887e2cc5e62b38cc9ee266094730e292
AbhishekV9 <avishekv444@gmail.com>
2021-06-12 14:41:08 +0530

    setup view engine

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const port=8000;
 //use express router:-we are telling here that any request comes in is handeled by this route
 app.use('/',require('./routes/index'));//i can write just './routes' also because it by default fetches index
 
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
       //  console.log('Error:',err);
