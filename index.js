58e8a42881ad33e205972e0c99f1469bd274aea8
Abhishek <avishekv444@gmail.com>
2021-07-03 21:46:18 +0530

    implimented sass for styling and given style to header and footer

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ const session=require('express-session'); //used for session cookie
 const passport=require('passport');//both lines are used for passport Authentication
 const passportLocal=require('./config/passport-local-strategy');
 const MongoStore=require('connect-mongo')(session); //this library requires an argument wich is the express session
+// const MongoStore =require('connect-mongo').default; newer version
+const sassMiddleware=require('node-sass-middleware');//requiring sass
 
-// const MongoStore =require('connect-mongo').default; //newer version
+
+app.use(sassMiddleware({ //use it just before the server is starting because i need those files to be precompiled before the server starts
+  src:'./assets/scss',                   //from where i will pick scss file to convert it into css
+  dest:'./assets/css',                   //where do i need to put my css file
+  debug:true,                           //if there is some error i want to show it on terminal
+  outputStyle:'extended',               // do i want to see it in single line or multiple line? offcorse multiple line
+  prefix:'/css',                        // prefix is basically where do i lookout,since we are using this middleware so where my server should look for css file
+}));
 
 app.use(express.urlencoded());
 
