f1d3e17c46862d1554deb0815e2c1dc9ab963687
Abhishek <avishekv444@gmail.com>
2021-07-09 17:07:42 +0530

    check authentication on creating posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,11 @@
 const express=require('express');
 const router=express.Router();
+const passport=require('passport');
 
 const postController=require('../controllers/post_controller');
 
-router.post('/create',postController.create);
+router.post('/create',passport.checkAuthentication,postController.create); //2nd level check u 
+//can create post only if you are authenticated and 1st level check is in home .ejs  
+//showing form only to the user who is authenticated
 
 module.exports=router;
